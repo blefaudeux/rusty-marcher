5d43e10cbfd14ef71f65f7af3de12b2a77222292
ben <benjamin.lefaudeux@gmail.com>
2020-02-04 21:11:02 -0800

    Revert "WIP rewriting in pure GTK, removing Relm"
    This reverts commit 0aecc5ed386ea9b0667313cc6297c103b5944dee.

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,8 +1,12 @@
+#[macro_use]
+extern crate relm;
+
+#[macro_use]
+extern crate relm_derive;
+
 extern crate gdk;
 extern crate gdk_pixbuf;
-extern crate gio;
 extern crate gtk;
-extern crate log;
 
 mod framebuffer;
 mod geometry;
@@ -17,13 +21,17 @@ mod sphere;
 mod triangle;
 
 use gdk_pixbuf::Pixbuf;
-use gio::prelude::*;
 use gtk::prelude::*;
-use std::sync::Arc;
-use std::thread;
-use {std::sync::mpsc::channel, std::sync::mpsc::Receiver, std::sync::mpsc::Sender};
+use gtk::*;
+use relm::{Relm, Update, Widget};
 
-#[derive(Clone, Copy)]
+#[allow(dead_code)]
+struct Model {
+    relm: Relm<Win>,
+    started_rendering: Option<renderer::Renderer>,
+}
+
+#[derive(Msg, Clone, Copy)]
 enum Msg {
     Quit,
     ToggleDefaultScene,
@@ -39,120 +47,28 @@ enum Msg {
 }
 
 // Create the structure that holds the widgets used in the view.
-struct RenderBackbone {
-    state_label: Arc<gtk::Label>,
-    image: Arc<gtk::Image>,
-    fb: Arc<framebuffer::FrameBuffer>,
-    renderer: Option<Arc<renderer::Renderer>>,
-    scene: Arc<scene::Scene>,
+struct Win {
+    state_label: Label,
+    image: Image,
+    model: Model,
+    window: Window,
+    fb: framebuffer::FrameBuffer,
+    scene: scene::Scene,
 }
 
-impl RenderBackbone {
-    pub fn new() -> Self {
-        // Create the raytrace framebuffer
-        RenderBackbone {
-            state_label: Arc::new(gtk::Label::new(Some("Waiting to create the renderer"))),
-            image: Arc::new(gtk::Image::new()),
-            fb: Arc::new(framebuffer::create_frame_buffer(1280, 800)),
-            renderer: None,
-            scene: Arc::new(scene::Scene::create_default()),
+impl Update for Win {
+    // Specify the model used for this widget.
+    type Model = Model;
+    // Specify the model parameter used to init the model.
+    type ModelParam = ();
+    // Specify the type of the messages sent to the update function.
+    type Msg = Msg;
+
+    fn model(relm: &Relm<Self>, _: ()) -> Model {
+        Model {
+            relm: relm.clone(),
+            started_rendering: None,
         }
-    }
-
-    pub fn arc_clone(&self) -> Self {
-        // Create a linked copy of the current RenderBackbone
-        RenderBackbone {
-            state_label: self.state_label.clone(),
-            image: self.image.clone(),
-            fb: self.fb.clone(),
-            renderer: None,
-            scene: self.scene.clone(),
-        }
-    }
-
-    pub fn build_ui(
-        &mut self,
-        app: &gtk::Application,
-        sender: Sender<Msg>,
-    ) -> gtk::ApplicationWindow {
-        // All encompassing window
-        let window = gtk::ApplicationWindow::new(app);
-        window.add_events(gdk::EventMask::SCROLL_MASK | gdk::EventMask::BUTTON_PRESS_MASK);
-
-        // Create the view using the normal GTK+ method calls.
-        let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
-        vbox.add(self.state_label.as_ref());
-
-        // - toolbar
-        let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-
-        // -- quick lambda to automate button declaration and binding
-        let add_button = |container: &gtk::Box, label: &str, msg: Msg, pipe: &Arc<Sender<Msg>>| {
-            let toggle = gtk::Button::new_with_label(label);
-            // Clone the shared pointer, needs to be owned per lambda instance
-            let pipe = pipe.clone();
-            toggle.connect_button_press_event(move |_, _| {
-                pipe.send(msg).unwrap();
-                gtk::Inhibit(false)
-            });
-            container.add(&toggle);
-        };
-
-        // -- add all the buttons we're interested in
-        {
-            let sender = Arc::new(sender);
-            add_button(&hbox, "Default scene", Msg::ToggleDefaultScene, &sender);
-            add_button(&hbox, "Save to file", Msg::ToggleSaveToFile, &sender);
-            add_button(&hbox, "Open file", Msg::ToggleOpenFile, &sender);
-
-            add_button(&hbox, "Left", Msg::ToggleMoveLeft, &sender);
-            add_button(&hbox, "Right", Msg::ToggleMoveRight, &sender);
-            add_button(&hbox, "Up", Msg::ToggleMoveUp, &sender);
-            add_button(&hbox, "Down", Msg::ToggleMoveDown, &sender);
-            vbox.add(&hbox);
-            vbox.add(self.image.as_ref());
-            window.add(&vbox);
-        }
-
-        // Handle keyboard events
-        window.connect_key_press_event(move |win, key| {
-            if let Some(key) = gdk::keyval_name(key.get_keyval()) {
-                match key.as_str() {
-                    "q" => {
-                        if let Some(app) = win.get_application() {
-                            log::info!("quitting!");
-                            app.quit()
-                        }
-                    }
-                    "t" => { /* TODO keep track if we're above or not */ }
-                    _ => {}
-                };
-            }
-            gtk::Inhibit(false)
-        });
-
-        // Handle mouse events
-        {
-            let pipe = sender.clone();
-            window.connect_scroll_event(move |_, ev| {
-                if ev.get_direction() == gdk::ScrollDirection::Up {
-                    pipe.send(Msg::ToggleMoveBack).unwrap();
-                } else if ev.get_direction() == gdk::ScrollDirection::Down {
-                    pipe.send(Msg::ToggleMoveCloser).unwrap();
-                }
-                Inhibit(false)
-            });
-        }
-
-        {
-            let pipe = sender.clone();
-            window.connect_delete_event(move |_, _| {
-                pipe.send(Msg::Quit).unwrap();
-                Inhibit(false)
-            });
-        }
-
-        window
     }
 
     fn update(&mut self, event: Msg) {
@@ -167,40 +83,39 @@ impl RenderBackbone {
             }
             Msg::Quit => gtk::main_quit(),
             Msg::ToggleOpenFile => {
-                return;
-                // // Open file dialog
-                // let open_file_dialog = FileChooserDialog::with_buttons(
-                //     Some("Open .obj file"),
-                //     Some(&self.core),
-                //     FileChooserAction::Open,
-                //     &[
-                //         ("_Cancel", ResponseType::Cancel),
-                //         ("_Open", ResponseType::Accept),
-                //     ],
-                // );
+                // Open file dialog
+                let open_file_dialog = FileChooserDialog::with_buttons(
+                    Some("Open .obj file"),
+                    Some(&self.window),
+                    FileChooserAction::Open,
+                    &[
+                        ("_Cancel", ResponseType::Cancel),
+                        ("_Open", ResponseType::Accept),
+                    ],
+                );
 
-                // // Filter: we just want .obj files
-                // let filter = FileFilter::new();
-                // filter.add_pattern("*.obj");
-                // open_file_dialog.set_filter(&filter);
+                // Filter: we just want .obj files
+                let filter = FileFilter::new();
+                filter.add_pattern("*.obj");
+                open_file_dialog.set_filter(&filter);
 
-                // let button_pressed = open_file_dialog.run();
-                // let path = open_file_dialog.get_filename();
-                // open_file_dialog.destroy();
+                let button_pressed = open_file_dialog.run();
+                let path = open_file_dialog.get_filename();
+                open_file_dialog.destroy();
 
-                // match path {
-                //     Some(filepath) => {
-                //         println!("Got {:?} from FileChooserDialog", filepath);
-                //         if button_pressed == ResponseType::Accept.into() {
-                //             self.open_obj(filepath);
-                //         }
-                //     }
-                //     None => {}
-                // }
+                match path {
+                    Some(filepath) => {
+                        println!("Got {:?} from FileChooserDialog", filepath);
+                        if button_pressed == ResponseType::Accept.into() {
+                            self.open_obj(filepath);
+                        }
+                    }
+                    None => {}
+                }
             }
             Msg::Sink => {}
             Msg::ToggleDefaultScene => {
-                self.scene.borrow() = scene::Scene::create_default();
+                self.scene = scene::Scene::create_default();
                 self.new_renderer();
                 self.update_raytrace_image();
             }
@@ -254,13 +169,99 @@ impl RenderBackbone {
             }
         }
     }
+}
 
+impl Widget for Win {
+    // Specify the type of the root widget.
+    type Root = Window;
+
+    // Return the root widget.
+    fn root(&self) -> Self::Root {
+        self.window.clone()
+    }
+
+    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        // Create the view using the normal GTK+ method calls.
+        let vbox = gtk::Box::new(Orientation::Vertical, 0);
+        let state_label = Label::new(Some("Waiting to create the renderer"));
+        vbox.add(&state_label);
+
+        // - toolbar
+        let hbox = gtk::Box::new(Orientation::Horizontal, 0);
+
+        // -- quick lambda to automate button declaration and binding
+        let add_button = |container: &gtk::Box, label: &str, event: Msg| {
+            let toggle = Button::new_with_label(label);
+            container.add(&toggle);
+            connect!(relm, toggle, connect_clicked(_), event);
+        };
+        // -- add all the buttons we're interested in
+        add_button(&hbox, "Default scene", Msg::ToggleDefaultScene);
+        add_button(&hbox, "Save to file", Msg::ToggleSaveToFile);
+        add_button(&hbox, "Open file", Msg::ToggleOpenFile);
+
+        add_button(&hbox, "Left", Msg::ToggleMoveLeft);
+        add_button(&hbox, "Right", Msg::ToggleMoveRight);
+        add_button(&hbox, "Up", Msg::ToggleMoveUp);
+        add_button(&hbox, "Down", Msg::ToggleMoveDown);
+        vbox.add(&hbox);
+
+        // Actual display view
+        let image = Image::new();
+        vbox.add(&image);
+
+        // All encompassing window
+        let window = Window::new(WindowType::Toplevel);
+        window.add(&vbox);
+        window.show_all();
+
+        // Grab the mouse events
+        window.add_events(gdk::EventMask::SCROLL_MASK);
+
+        connect!(
+            relm,
+            window,
+            connect_scroll_event(_, evt),
+            return (
+                if evt.get_direction() == gdk::ScrollDirection::Up {
+                    Msg::ToggleMoveBack
+                } else if evt.get_direction() == gdk::ScrollDirection::Down {
+                    Msg::ToggleMoveCloser
+                } else {
+                    Msg::Sink
+                },
+                Inhibit(false)
+            )
+        );
+
+        // Create the raytrace framebuffer
+        let fb = framebuffer::create_frame_buffer(1280, 800);
+
+        connect!(
+            relm,
+            window,
+            connect_delete_event(_, _),
+            return (Some(Msg::Quit), Inhibit(false))
+        );
+
+        Win {
+            state_label: state_label,
+            image: image,
+            model,
+            window: window,
+            fb: fb,
+            scene: scene::Scene::create_default(),
+        }
+    }
+}
+
+impl Win {
     fn open_obj(&mut self, filepathbuf: std::path::PathBuf) {
         let path = filepathbuf.into_os_string().into_string();
         match path {
             Ok(filepath) => {
                 // Make sure the renderer is started
-                if !self.renderer.is_some() {
+                if !self.model.started_rendering.is_some() {
                     self.new_renderer()
                 }
 
@@ -312,7 +313,7 @@ impl RenderBackbone {
                 ));
 
                 println!["Opened file successfuly"];
-                self.scene = Arc::new(scene);
+                self.scene = scene;
 
                 // Re-run the raytracer
                 self.update_raytrace_image();
@@ -324,8 +325,8 @@ impl RenderBackbone {
     }
 
     fn update_raytrace_image(&mut self) {
-        if self.renderer.is_some() {
-            let raymarcher = self.renderer.as_mut().unwrap();
+        if self.model.started_rendering.is_some() {
+            let raymarcher = self.model.started_rendering.as_mut().unwrap();
             self.state_label
                 .set_text(&raymarcher.render(&mut self.fb, &self.scene).to_string());
 
@@ -348,7 +349,7 @@ impl RenderBackbone {
     }
 
     fn save_to_file(&mut self) {
-        if self.renderer.is_some() {
+        if self.model.started_rendering.is_some() {
             self.fb.normalize();
             self.fb.write_ppm("out.ppm").unwrap();
             self.state_label.set_text("Saved rendered file");
@@ -361,49 +362,19 @@ impl RenderBackbone {
     fn new_renderer(&mut self) {
         // Create the renderer
         // FIXME: the fb is only use for sizing purposes, should be cleaned
-        let ray_marcher = Arc::new(renderer::create_renderer(
-            1.5,
-            self.fb.height as f64,
-            self.fb.width as f64,
-        ));
-        self.renderer = Some(ray_marcher);
+        let ray_marcher =
+            renderer::create_renderer(1.5, self.fb.height as f64, self.fb.width as f64);
+        self.model.started_rendering = Some(ray_marcher);
         self.state_label.set_text("Created new ray tracing engine");
     }
 
     #[allow(dead_code)]
     fn clear_renderer(&mut self) {
-        self.renderer = None;
+        self.model.started_rendering = None;
         self.state_label.set_text("Cleared renderer");
     }
 }
 
 fn main() {
-    // Start up the GTK3 subsystem.
-    gtk::init().expect("Unable to start GTK3. Error");
-
-    // Create the actual app
-    let application = gtk::Application::new(Some("rusty.marcher"), Default::default())
-        .expect("failed to initialize GTK application");
-
-    // Handle all callbacks
-    let (sender, receiver) = channel::<Msg>();
-
-    let mut backbone = Arc::<RenderBackbone>::new(RenderBackbone::new());
-
-    // Hook up to references of the different objects, these will survive the
-    // scope through the move into the returned struct
-    {
-        let backbone_callback = backbone.clone();
-        thread::spawn(move || {
-            let msg = receiver.recv().unwrap();
-            backbone_callback.update(msg);
-        });
-    }
-
-    // Activate and run
-    application.connect_activate(move |app| {
-        backbone.build_ui(&app, sender).show_all();
-    });
-
-    application.run(&[]);
+    Win::run(()).unwrap();
 }
